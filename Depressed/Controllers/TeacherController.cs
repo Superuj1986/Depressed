cbb127144f0063378c73ac43360bbda064c614f4
Superuj1986 <dotuanlong2209@gmail.com>
2024-04-01 11:47:36 +0700

    Chỉn chu và hoàn thiện code

--- Depressed/Controllers/TeacherController.cs
+++ Depressed/Controllers/TeacherController.cs
@@ -132,7 +132,6 @@ namespace Depressed.Controllers
                 .Include(x => x.ClassMembers)
                 .Include(y => y.Lichhocs)
                 .FirstOrDefault(z => z.class_id == id);
-            var check = null;
             return View(lophoc);
         }
         [HttpPost]
